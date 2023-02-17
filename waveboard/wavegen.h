f74e075067ffde024797ce46049e854cc0dcb522
Niko <niko.scher.26@gmail.com>
2023-02-17 15:09:40 +0000

    Added additional comments

--- waveboard/wavegen.h
+++ waveboard/wavegen.h
@@ -10,10 +10,10 @@
 #define __WAVEGEN_H__
 
 // Pin numbers
-#define SIGA_PIN    0   // Signal A: Output to LED or oscilloscope
-#define SIGB_PIN    1   // Signal B: Output, static waveform
-#define SWITCH1_PIN 5   // Switch 1: Enable/disable pulse stream
-#define SWITCH2_PIN 6   // Switch 2: Run normal/modified pulse stream
+#define SIGA_PIN        1   // Signal A: Output to LED or oscilloscope
+#define SIGB_PIN        0   // Signal B: Output, static waveform
+#define SWITCHACT_PIN   5   // Switch 1: Enable/disable pulse stream
+#define SWITCHMODE_PIN  6   // Switch 2: Run normal/modified pulse stream
 
 // Unique parameters to construct signal A
 #define PWIDTH 800  // s = 8: 8 * 100us
@@ -21,10 +21,10 @@
 #define PNUM   12   // h = 8: 8 + 4 pulses
 #define PBGAP  2500 // e = 5: 5 * 500us
 #define MODE   2    // r = 9: rem(9 / 4) + 1
-#define PITER  50   // BLAH
+#define PITER  50   // Gap in us between each pulse in pulse stream
 
-#define SIGBWIDTH 50   // Width of pulse of Signal B in us
-#define DMOD      1000 // Debug modifier: Multiplies whole signal by constant
+#define SIGBWIDTH 50      // Width of pulse of Signal B in us
+#define DMOD      100000  // Debug modifier: Multiplies whole signal by constant
 
 // Includes for debugging
 //#include <stdio.h>
@@ -35,7 +35,7 @@ typedef unsigned char uint8;
 typedef unsigned int uint16;
 
 // Function prototypes to generate signal A & B
-void genSigA(uint16 pWidth, uint16 pSpace, uint8 pNum, uint16 pbGap, uint8 mode, bool debug);
-void genSigB(uint16 pWidth, bool debug);
+void genSigA(uint16 pWidth, uint16 pSpace, uint8 pNum, uint16 pbGap, uint8 mode, uint8 pIter);
+void genSigB(uint16 pWidth);
 
 #endif  // __WAVEGEN_H__
